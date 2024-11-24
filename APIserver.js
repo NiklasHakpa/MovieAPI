aa08b71d4fdeb212ffbd2e0e25df33f1f710d431
NiklasHakpa <n3hani01@students.oamk.fi>
2024-11-24 13:12:17 +0200

    genre endpoints

--- APIserver.js
+++ APIserver.js
@@ -11,3 +11,14 @@ app.listen(3001, () => {
     console.log("Server is running in port 3001");
     
 })
+
+//GET all genres
+app.get('/genres', (req,res) => {
+
+})
+
+//Add new genre maybe post
+app.post('/genres', (req,res) => {
+
+})
+
