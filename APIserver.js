12ed67abc94801b2c81354acba50797f6d893d99
NiklasHakpa <n3hani01@students.oamk.fi>
2024-11-24 13:31:26 +0200

    movie endpoints added + small tweaks in genre

--- APIserver.js
+++ APIserver.js
@@ -9,16 +9,30 @@ app.use(express.json());
 
 app.listen(3001, () => {
     console.log("Server is running in port 3001");
-    
-})
-
-//GET all genres
-app.get('/genres', (req,res) => {
-
-})
+});
+        //!!GENRES!!//
 
 //Add new genre maybe post
 app.post('/genres', (req,res) => {
+    //{"genre_name": "Action"}//
+    //maybe like this
+});
+//GET all genres
+app.get('/genres', (req,res) => {
+});
 
-})
 
+        //!!MOVIES!!//
+//Add new movie POST
+app.post('/movies', (req,res) => {
+    //{ maybe like a this
+        //"movie_name": "Inception",
+        //"movie_year": 2010,
+        //"genre_id": 1}
+});
+//retrieve all movies GET
+app.get('/movies', (req,res) => {
+});
+//Retrieve specific movie by id GET
+app.get('/movies/{movie_id}', (req,res) => {
+});
